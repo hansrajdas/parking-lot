8fc3a7c70a2c567606228e93a52b15eb6492afb7
Hansraj Das <raj.das.136@gmail.com>
2018-11-26 23:45:41 +0530

    command parser and validator added

--- src/commands/parser.py
+++ src/commands/parser.py
@@ -1,3 +1,30 @@
+SUPPORTED_COMMANDS = {
+    'create_parking_lot': 1,
+    'park': 2,
+    'leave': 1,
+    'status': 0,
+    'registration_numbers_for_cars_with_colour': 1,
+    'slot_numbers_for_cars_with_colour': 1,
+    'slot_number_for_registration_number': 1,
+}
+
 class Parser:
-    """Parses parking commands."""
-    pass
+    """Parses parking lot commands."""
+
+    def parse_command(self, command):
+        """Parses given command."""
+        if not command:
+            print '[Error]: Empty command received'
+            return False
+        return self.validate_command(command.strip().split(' '))
+
+    def validate_command(self, command):
+        """Validates if command is valid or not."""
+        if command[0] not in SUPPORTED_COMMANDS:
+            print '[Error]: Command not supported - %s' % command[0]
+            return False
+        elif SUPPORTED_COMMANDS[command[0]] != len(command) - 1:
+            print '[Error]: Invalid number of arguments for command: %s' % (
+            command[0])
+            return False
+        return command
--- src/interface.py
+++ src/interface.py
@@ -1,11 +1,14 @@
 from commands.parser import Parser
+from requests.requests import Request
 
 def read_commands(filename):
     """Read commands from file and sends for processing."""
     file_fp = open(filename, 'r')
     command = file_fp.readline()
     while command:
-        print command
-        Parser.handle_request(command)
+        parsed_command = Parser.parse_command(command)
+        if parsed_command:
+            Request.handle_request(parsed_command)
+        print parsed_command
         command = file_fp.readline()
     file_fp.close()
