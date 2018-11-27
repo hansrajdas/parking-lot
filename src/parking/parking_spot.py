38f20dac92046b0a8a258fff9491b7ae0fbdf018
Hansraj Das <raj.das.136@gmail.com>
2018-11-27 23:14:05 +0530

    parking spot and vehicle entities updated

--- src/parking/parking_spot.py
+++ src/parking/parking_spot.py
@@ -2,3 +2,14 @@ class ParkingSpot:
     def __init__(self, spot_num):
         self.spot_num = spot_num
         self.vehicle = None
+
+    def get_spot_num(self):
+        return self.spot_num
+
+    def get_vehicle_parked(self):
+        """Returns the details of vehicle parked at this spot."""
+        return self.vehicle
+
+    def park_vehicle(self, vehicle):
+        """Parks a vehicle at this spot."""
+        self.vehicle = vehicle
--- src/parking/vehicle.py
+++ src/parking/vehicle.py
@@ -2,3 +2,15 @@ class Vehicle:
     def __init__(self, registration_num, color):
         self.registration_num = registration_num
         self.color = color
+
+    def update_registration_number(self, registration_num):
+        self.registration_num = registration_num
+
+    def update_color(self, color):
+        self.color = color
+
+    def get_registration_number(self):
+        return self.registration_num
+
+    def get_color(self):
+        return self.color
