627e96f073fc1324bb7e1265b0223359dff0a732
Hansraj Das <raj.das.136@gmail.com>
2018-11-28 20:36:50 +0530

    min heap package added

--- src/parking/parking_lot.py
+++ src/parking/parking_lot.py
@@ -2,25 +2,25 @@ import utils
 
 from parking.parking_spot import ParkingSpot
 from parking.vehicle import Vehicle
-from min_heap import MinHeap
+from storage.min_heap import MinHeap
 
 class ParkingLot:
     """Manages parking lot."""
     def __init__(self, number_of_slots):
         self.free_slots = number_of_slots
-        self.slot_heap = MinHeap(number_of_slots)
+        self.slots = MinHeap(number_of_slots)
         self.vehicles = {}  # Maps parking spot to vehicle
         utils.console_log(
-            'Created a parking lot with %d slots\n' % number_of_slots)
+            'Created a parking lot with %d slots' % number_of_slots)
 
-    def get_next_free_slot(self):
-        return self.slot_heap.extract_min(self.free_slots)
+    def _get_next_free_slot(self):
+        return self.slots.extract_min(self.free_slots)
 
     def park_vehicle(self, registration_number, color):
         if not self.free_slots:
             utils.console_log('Sorry, parking lot is full')
             return None
-        spot = self.get_next_free_slot()
+        spot = self._get_next_free_slot()
         self.vehicles[spot.spot_num] = Vehicle(registration_number, color)
         self.free_slots -= 1
         utils.console_log('Allocated slot number: %d' % spot.spot_num)
---
+++ src/storage/min_heap.py
@@ -0,0 +1,26 @@
+from parking.parking_spot import ParkingSpot
+
+class MinHeap:
+    def __init__(self, n):
+        self.slots = [ParkingSpot(i + 1) for i in range(n)]
+
+    def min_heapify(self, n, idx):
+        min_idx = idx
+        left_idx = 2*idx + 1
+        right_idx = 2*idx + 2
+
+        if left_idx < n and self.slots[min_idx] > self.slots[left_idx]:
+            min_idx = left_idx
+        if right_idx < n and self.slots[min_idx] > self.slots[right_idx]:
+            min_idx = right_idx
+
+        if min_idx != idx:
+            temp = self.slots[idx]
+            self.slots[idx] = self.slots[min_idx]
+            self.slots[min_idx] = temp
+            self.min_heapify(n, min_idx)
+
+    def extract_min(self, n):
+        self.slots[0], self.slots[n - 1] = self.slots[n - 1], self.slots[0]
+        self.min_heapify(n - 1, 0)
+        return self.slots[n - 1]
