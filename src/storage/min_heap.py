cf7bb5214eee5a745a0ebef2d9e4ecb85deb0c28
Hansraj Das <raj.das.136@gmail.com>
2018-11-28 23:37:57 +0530

    shift up logic added in min heap

--- src/storage/min_heap.py
+++ src/storage/min_heap.py
@@ -6,13 +6,14 @@ class MinHeap:
         self.slots = [ParkingSpot(i + 1) for i in range(n)]
 
     def min_heapify(self, n, idx):
+        """Min heapfies parking slots based on ids."""
         min_idx = idx
         left_idx = 2*idx + 1
         right_idx = 2*idx + 2
 
-        if left_idx < n and self.slots[min_idx] > self.slots[left_idx]:
+        if left_idx < n and self.slots[min_idx].id > self.slots[left_idx].id:
             min_idx = left_idx
-        if right_idx < n and self.slots[min_idx] > self.slots[right_idx]:
+        if right_idx < n and self.slots[min_idx].id > self.slots[right_idx].id:
             min_idx = right_idx
 
         if min_idx != idx:
@@ -21,12 +22,23 @@ class MinHeap:
             self.slots[min_idx] = temp
             self.min_heapify(n, min_idx)
 
+    def shift_up(self, node_idx):
+        """Shits node up in heap if new element added breaks heap property."""
+        if node_idx:
+            parent_idx = (node_idx - 1)/2
+            if self.slots[parent_idx].id > self.slots[node_idx].id:
+                buf = self.slots[parent_idx]
+                self.slots[parent_idx] = self.slots[node_idx]
+                self.slots[node_idx] = buf
+                self.shift_up(parent_idx)
+
     def extract_min(self, n):
+        """Extracts and returns min parking slot number from free slots."""
         self.slots[0], self.slots[n - 1] = self.slots[n - 1], self.slots[0]
         self.min_heapify(n - 1, 0)
         return self.slots[n - 1]
 
     def add_to_heap(self, n, spot_num):
+        """Adds a new parking spot to heap."""
         self.slots[n].id = spot_num
-        self.slots[0], self.slots[n] = self.slots[n], self.slots[0]
-        self.min_heapify(n + 1, 0)
+        self.shift_up(n)
